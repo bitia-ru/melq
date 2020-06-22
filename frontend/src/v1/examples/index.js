78568736396ffd6dec301efabf7542beb085f5a4
Sergey <vertigomaill@gmail.com>
2020-06-22 16:34:45 +0300

    MELQ-45: Add component Switch

--- frontend/src/v1/examples/index.js
+++ frontend/src/v1/examples/index.js
@@ -1,9 +1,11 @@
 import ButtonExample from './ButtonExample';
+import SwitchExample from './SwitchExample';
 import TooltipExample from './TooltipExample';
 import CounterExample from './CounterExample';
 
 const examples = {
   button: ButtonExample,
+  switch: SwitchExample,
   tooltip: TooltipExample,
   counter: CounterExample,
 };
