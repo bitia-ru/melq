4e2f77df6552e75c87ffe2c3c9fb1187d51f9e7e
Artem Levenkov <alevenkov@networkoptix.com>
2020-06-30 00:37:58 +0300

    MELQ-56: Correct rubocop issues

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,26 +1,26 @@
 User.create!(
   email: 'root@email.ru',
-  password_digest: BCrypt::Password.create("123456"),
+  password_digest: BCrypt::Password.create('123456')
 )
 
 Setting.create!(
-    nickname: 'Nickname',
-    about: 'About',
-    in_development: true,
-    access_key: '123456',
-    copyright_year: '2020',
-    about_blog_slug: 'about-blog-slug',
-    privacy_policy_slug: 'privacy-policy-slug'
+  nickname: 'Nickname',
+  about: 'About',
+  in_development: true,
+  access_key: '123456',
+  copyright_year: '2020',
+  about_blog_slug: 'about-blog-slug',
+  privacy_policy_slug: 'privacy-policy-slug'
 )
 
 Post.create!(
-    slug: 'about-blog-slug',
-    title: 'About blog',
-    content: 'About blog'
+  slug: 'about-blog-slug',
+  title: 'About blog',
+  content: 'About blog'
 )
 
 Post.create!(
-    slug: 'privacy-policy-slug',
-    title: 'Privacy policy',
-    content: 'Privacy policy'
+  slug: 'privacy-policy-slug',
+  title: 'Privacy policy',
+  content: 'Privacy policy'
 )
