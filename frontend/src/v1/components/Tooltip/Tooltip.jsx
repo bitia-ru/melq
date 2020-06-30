1a29114c254a46ef02f59c1185919049d3df9f10
Sergey <vertigomaill@gmail.com>
2020-06-30 23:53:39 +0300

    MELQ-58: Introduce Tooltip component

---
+++ frontend/src/v1/components/Tooltip/Tooltip.jsx
@@ -0,0 +1,148 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from '../../aphrodite';
+import { defaultColor, focusBgColor, focusBorderColor } from '../../theme';
+
+const styles = StyleSheet.create({
+  tooltipWrapper: {
+    position: 'relative',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+  },
+  tooltip: {
+    position: 'absolute',
+    display: 'flex',
+    padding: '5px',
+    borderRadius: '3px',
+    backgroundColor: focusBgColor,
+    minWidth: '120px',
+    maxWidth: '200px',
+    color: defaultColor,
+    border: `1px solid ${focusBorderColor}`,
+    zIndex: '999',
+    ':before': {
+      content: '\'\'',
+      position: 'absolute',
+      borderWidth: '10px',
+      borderStyle: 'solid',
+      zIndex: '998',
+    },
+    ':after': {
+      content: '\'\'',
+      position: 'absolute',
+      display: 'block',
+      borderWidth: '10px',
+      borderStyle: 'solid',
+      zIndex: '998',
+    },
+  },
+  topSideTooltip: {
+    justifyContent: 'center',
+    bottom: 'calc(100% + 20px)',
+    ':before': {
+      bottom: '-20px',
+      borderColor: `${focusBorderColor} transparent transparent transparent`,
+    },
+    ':after': {
+      bottom: '-19px',
+      borderColor: `${focusBgColor} transparent transparent transparent`,
+    },
+  },
+  rightSideTooltip: {
+    alignItems: 'center',
+    left: 'calc(100% + 20px)',
+    ':before': {
+      left: '-20px',
+      borderColor: `transparent ${focusBorderColor} transparent transparent`,
+    },
+    ':after': {
+      left: '-19px',
+      borderColor: `transparent ${focusBgColor} transparent transparent`,
+    },
+  },
+  bottomSideTooltip: {
+    justifyContent: 'center',
+    top: 'calc(100% + 20px)',
+    ':before': {
+      top: '-20px',
+      borderColor: `transparent transparent ${focusBorderColor} transparent`,
+    },
+    ':after': {
+      top: '-19px',
+      borderColor: `transparent transparent ${focusBgColor} transparent`,
+    },
+  },
+  leftSideTooltip: {
+    alignItems: 'center',
+    right: 'calc(100% + 20px)',
+    ':before': {
+      right: '-20px',
+      borderColor: `transparent transparent transparent ${focusBorderColor}`,
+    },
+    ':after': {
+      right: '-19px',
+      borderColor: `transparent transparent transparent ${focusBgColor}`,
+    },
+  },
+});
+
+const getTooltipSide = (tooltipSide) => {
+  switch (tooltipSide) {
+  case 'top': {
+    return styles.topSideTooltip;
+  }
+  case 'bottom': {
+    return styles.bottomSideTooltip;
+  }
+  case 'left': {
+    return styles.leftSideTooltip;
+  }
+  case 'right':
+  default: {
+    return styles.rightSideTooltip;
+  }
+  }
+};
+
+const Tooltip = ({ tooltipText, tooltipSide, children, isShowing }) => {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    if (!isShowing || isShowing !== visible) {
+      setVisible(isShowing);
+    }
+  }, [isShowing]);
+
+  const showTooltip = () => {
+    setVisible(true);
+  };
+
+  const hideTooltip = () => {
+    setVisible(false);
+  };
+
+  return (
+    <span className={css(styles.tooltipWrapper)}>
+      {
+        visible
+        && <span className={css(styles.tooltip, getTooltipSide(tooltipSide))}>
+          {tooltipText}
+        </span>
+      }
+      <span onMouseEnter={showTooltip} onMouseLeave={hideTooltip}>
+        {children}
+      </span>
+    </span>
+  );
+};
+
+Tooltip.propTypes = {
+  tooltipText: PropTypes.string.isRequired,
+  tooltipSide: PropTypes.oneOf(['top', 'right', 'bottom', 'left']),
+  children: PropTypes.node.isRequired,
+  isShowing: PropTypes.bool,
+};
+
+export default Tooltip;
---
+++ frontend/src/v1/examples/TooltipExample.js
@@ -0,0 +1,139 @@
+import React, { useState } from 'react';
+import Tooltip from '../components/Tooltip/Tooltip';
+import { StyleSheet, css } from '../aphrodite';
+import { successColor, separatorColor, defaultColor, themeStyles } from '../theme';
+import check from './assets/check.svg';
+
+const styles = StyleSheet.create({
+  checkboxWrapper: {
+    width: '40px',
+    height: '44px',
+    background: 'white',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  checkbox: {
+    width: '24px',
+    height: '24px',
+    margin: '0px 8px',
+    background: separatorColor,
+    borderRadius: '3px',
+  },
+  svg: { margin: '7px 5px 8px 6px' },
+  active: {
+    background: successColor,
+    backgroundImage: `url(${check})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: '50% 50%',
+  },
+  title: { color: defaultColor },
+  buttonWrapper: {
+    display: 'inline-block',
+    position: 'relative',
+    margin: '0px 20px',
+  },
+  button: {
+    backgroundColor: successColor,
+    border: 'none',
+    color: 'white',
+    padding: '10px 10px',
+    textAlign: 'center',
+    textDecoration: 'none',
+    display: 'inline-block',
+    fontSize: '16px',
+    margin: '4px 2px',
+    cursor: 'pointer',
+  },
+  containerWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '200px',
+    height: '180px',
+  },
+  label: {
+    width: '40px',
+    height: '44px',
+  },
+});
+
+const TooltipExample = () => {
+  const [active, setActive] = useState(false);
+  const [tooltipText, setTooltipText] = useState('Checkbox Tooltip');
+
+  const tooltipTextOnChange = (e) => {
+    setTooltipText(e.target.value);
+  };
+
+  return (
+    <div>
+      <div className={css(styles.containerWrapper, styles.title, themeStyles.defaultFont)}>
+        <div className={css(styles.container)}>
+          <p>Top Tooltip</p>
+          <div className={css(styles.checkboxWrapper)}>
+            <Tooltip tooltipText={tooltipText} tooltipSide="top" isShowing={active}>
+              <div className={css(styles.checkbox, styles.active)} />
+            </Tooltip>
+          </div>
+        </div>
+
+        <div className={css(styles.container)}>
+          <p>Right Tooltip</p>
+          <div className={css(styles.checkboxWrapper)}>
+            <Tooltip tooltipText={tooltipText} tooltipSide="right" isShowing={active}>
+              <div className={css(styles.checkbox, styles.active)} />
+            </Tooltip>
+          </div>
+        </div>
+
+        <div className={css(styles.container)}>
+          <p>Bottom Tooltip</p>
+          <div className={css(styles.checkboxWrapper)}>
+            <Tooltip tooltipText={tooltipText} tooltipSide="bottom" isShowing={active}>
+              <div className={css(styles.checkbox, styles.active)} />
+            </Tooltip>
+          </div>
+        </div>
+
+        <div className={css(styles.container)}>
+          <p>Left Tooltip</p>
+          <div className={css(styles.checkboxWrapper)}>
+            <Tooltip tooltipText={tooltipText} tooltipSide="left" isShowing={active}>
+              <div className={css(styles.checkbox, styles.active)} />
+            </Tooltip>
+          </div>
+        </div>
+      </div>
+
+
+      <div className={css(styles.containerWrapper, styles.title, themeStyles.defaultFont)}>
+        <p>Click to activate all tooltip above</p>
+        <div className={css(styles.buttonWrapper)}>
+          <Tooltip tooltipText="Activate All Tooltip" tooltipSide="top">
+            <button
+              type="button"
+              className={css(styles.button)}
+              onClick={() => setActive(!active)}
+            >
+              Activate
+            </button>
+          </Tooltip>
+        </div>
+      </div>
+      <div className={css(styles.containerWrapper, styles.title, themeStyles.defaultFont)}>
+        <p>Set tooltip for all checkboxes</p>
+        <Tooltip tooltipText="Max word length=20" tooltipSide="right">
+          <input type="text" name="tooltip" id="tooltip" onChange={tooltipTextOnChange} />
+        </Tooltip>
+      </div>
+
+
+    </div>
+  );
+};
+
+export default TooltipExample;
