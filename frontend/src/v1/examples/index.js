8f89d9c6eadea958f0da3c319d3caf87fd9ca83f
Sergey <vertigomaill@gmail.com>
2020-06-30 23:53:39 +0300

    MELQ-58: Add Tooltip component

--- frontend/src/v1/examples/index.js
+++ frontend/src/v1/examples/index.js
@@ -1,5 +1,9 @@
 import ButtonExample from './ButtonExample';
+import TooltipExample from './TooltipExample';
 
-const examples = { button: ButtonExample };
+const examples = {
+  button: ButtonExample,
+  tooltip: TooltipExample,
+};
 
 export default examples;
