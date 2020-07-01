ed79770488100a425d06cf243be57b7bdbc0b055
Sergey <vertigomaill@gmail.com>
2020-07-01 19:06:41 +0300

    MELQ-58: Fix styles

--- frontend/src/v1/components/Tooltip/Tooltip.jsx
+++ frontend/src/v1/components/Tooltip/Tooltip.jsx
@@ -3,11 +3,9 @@ import PropTypes from 'prop-types';
 import { StyleSheet, css } from '../../aphrodite';
 
 const styles = StyleSheet.create({
-  tooltipText: {
+  tooltip: {
     visibility: 'hidden',
     width: '120px',
-    backgroundColor: '#52C4A9',
-    color: 'white',
     textAlign: 'center',
     borderRadius: '3px',
     padding: '5px 0',
@@ -16,24 +14,35 @@ const styles = StyleSheet.create({
     top: '120%',
     left: '50%',
     marginLeft: '-60px',
+    ':before': {
+      content: '\'\'',
+      position: 'absolute',
+      bottom: '100%',
+      left: '45%',
+      marginLeft: '-5px',
+      borderWidth: '10px',
+      borderStyle: 'solid',
+    },
     ':after': {
       content: '\'\'',
       position: 'absolute',
       bottom: '100%',
-      left: '50%',
+      left: '45%',
       marginLeft: '-5px',
-      borderWidth: '5px',
+      borderWidth: '10px',
       borderStyle: 'solid',
-      borderColor: 'transparent transparent #52C4A9 transparent',
+      top: '-18px',
     },
   },
 });
 
-const Tooltip = ({ tooltipText }) => (
-  <span className={css(styles.tooltipText)}>{tooltipText}</span>
+const Tooltip = ({ tooltipText, tooltipStyles }) => (
+  <span className={css(styles.tooltip, tooltipStyles)}>{tooltipText}</span>
 );
 
-
-Tooltip.propTypes = { tooltipText: PropTypes.string.isRequired };
+Tooltip.propTypes = {
+  tooltipText: PropTypes.string.isRequired,
+  tooltipStyles: PropTypes.string.isRequired,
+};
 
 export default Tooltip;
