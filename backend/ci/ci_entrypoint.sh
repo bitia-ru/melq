db5ff554ed823a256676719bf55c0d0bf3dcef3d
Artem Levenkov <alev@bitia.ru>
2020-07-02 22:03:41 +0300

    MELQ-64: Introduce RSpec and CI job for it

--- backend/ci/ci_entrypoint.sh
+++ backend/ci/ci_entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 
 cd /app
 
-#bin/rake db:create db:migrate RAILS_ENV=test
+bin/rake db:create db:migrate RAILS_ENV=test
 
-#bin/bundle exec rspec
+bin/bundle exec rspec
 
 bin/bundle exec rubocop
 
