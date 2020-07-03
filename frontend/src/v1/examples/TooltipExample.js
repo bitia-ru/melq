dfb360cd629175f6ec4ce071dddc405fb15a2971
Sergey <vertigomaill@gmail.com>
2020-07-03 21:42:04 +0300

    MELQ-58: Rework Tooltip

--- frontend/src/v1/examples/TooltipExample.js
+++ frontend/src/v1/examples/TooltipExample.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Tooltip from '../components/Tooltip/Tooltip';
 import { StyleSheet, css } from '../aphrodite';
 import { successColor, separatorColor, defaultColor, themeStyles } from '../theme';
@@ -6,22 +6,11 @@ import check from './assets/check.svg';
 
 const styles = StyleSheet.create({
   checkboxWrapper: {
-    display: 'inline-block',
-    position: 'relative',
     width: '40px',
     height: '44px',
     background: 'white',
-    margin: '10px 50px',
-    textAlign: 'center',
-    ':hover': { '>span': { visibility: 'visible' } },
-  },
-  buttonWrapper: {
-    display: 'inline-block',
-    position: 'relative',
-    background: 'white',
-    margin: '10px 50px',
-    textAlign: 'center',
-    ':hover': { '>span': { visibility: 'visible' } },
+    display: 'flex',
+    justifyContent: 'center',
   },
   checkbox: {
     width: '24px',
@@ -38,100 +27,107 @@ const styles = StyleSheet.create({
     backgroundPosition: '50% 50%',
   },
   title: { color: defaultColor },
+  buttonWrapper: {
+    display: 'inline-block',
+    position: 'relative',
+    margin: '0px 20px',
+  },
+  button: {
+    backgroundColor: successColor,
+    border: 'none',
+    color: 'white',
+    padding: '10px 10px',
+    textAlign: 'center',
+    textDecoration: 'none',
+    display: 'inline-block',
+    fontSize: '16px',
+    margin: '4px 2px',
+    cursor: 'pointer',
+  },
+  containerWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
   container: {
     display: 'flex',
-    ':hover': { '>span': { visibility: 'visible' } },
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '200px',
+    height: '180px',
+  },
+  label: {
+    width: '40px',
+    height: '44px',
   },
 });
 
-const TooltipExample = () => (
-  <>
-    <div className={css(styles.checkboxWrapper)}>
-      <div className={css(styles.title, themeStyles.defaultFont)}>V1</div>
-      <div className={css(styles.checkbox, styles.active)} />
-      <Tooltip
-        tooltipText="Checkbox#1 Tooltip"
-        tooltipSide="bottom"
-      />
-    </div>
+const TooltipExample = () => {
+  const [active, setActive] = useState(false);
+  const [tooltipText, setTooltipText] = useState('Checkbox Tooltip');
 
-    <div className={css(styles.checkboxWrapper)}>
-      <div className={css(styles.title, themeStyles.defaultFont)}>V2</div>
-      <div className={css(styles.checkbox, styles.active)} />
-      <Tooltip
-        tooltipText="Checkbox#2 Tooltip"
-        tooltipSide="bottom"
-      />
-    </div>
+  const tooltipTextOnChange = (e) => {
+    setTooltipText(e.target.value);
+  };
 
-    <div className={css(styles.checkboxWrapper)}>
-      <div className={css(styles.title, themeStyles.defaultFont)}>V3</div>
-      <div className={css(styles.checkbox, styles.active)} />
-      <Tooltip
-        tooltipText="Checkbox#3 Tooltip"
-        tooltipSide="bottom"
-      />
-    </div>
+  return (
+    <div>
+      <div className={css(styles.containerWrapper, styles.title, themeStyles.defaultFont)}>
+        <div className={css(styles.container)}>
+          <p>Top Tooltip</p>
+          <div className={css(styles.checkboxWrapper)}>
+            <Tooltip tooltipText={tooltipText} tooltipSide="top" isShowing={active}>
+              <div className={css(styles.checkbox, styles.active)} />
+            </Tooltip>
+          </div>
+        </div>
 
-    <div className={css(styles.checkboxWrapper)}>
-      <div className={css(styles.title, themeStyles.defaultFont)}>V4</div>
-      <div className={css(styles.checkbox, styles.active)} />
-      <Tooltip
-        tooltipText="Checkbox#4 Tooltip"
-        tooltipSide="bottom"
-      />
-    </div>
+        <div className={css(styles.container)}>
+          <p>Right Tooltip</p>
+          <div className={css(styles.checkboxWrapper)}>
+            <Tooltip tooltipText={tooltipText} tooltipSide="right" isShowing={active}>
+              <div className={css(styles.checkbox, styles.active)} />
+            </Tooltip>
+          </div>
+        </div>
 
-    <div className={css(styles.container)}>
-      <div className={css(styles.checkboxWrapper)}>
-        <div className={css(styles.checkbox, styles.active)} />
-        <Tooltip
-          tooltipText="Checkbox#4 Tooltip Tooltip Tooltip Tooltip Tooltip"
-          tooltipSide="right"
-        />
+        <div className={css(styles.container)}>
+          <p>Bottom Tooltip</p>
+          <div className={css(styles.checkboxWrapper)}>
+            <Tooltip tooltipText={tooltipText} tooltipSide="bottom" isShowing={active}>
+              <div className={css(styles.checkbox, styles.active)} />
+            </Tooltip>
+          </div>
+        </div>
+
+        <div className={css(styles.container)}>
+          <p>Left Tooltip</p>
+          <div className={css(styles.checkboxWrapper)}>
+            <Tooltip tooltipText={tooltipText} tooltipSide="left" isShowing={active}>
+              <div className={css(styles.checkbox, styles.active)} />
+            </Tooltip>
+          </div>
+        </div>
       </div>
-    </div>
 
-    <div className={css(styles.container)}>
-      <div className={css(styles.buttonWrapper)}>
-        <button>Send1</button>
-        <Tooltip
-          tooltipText="Tooltip"
-          tooltipSide="bottom"
-        />
-      </div>
-    </div>
 
-    <div className={css(styles.container)}>
-      <div className={css(styles.buttonWrapper)}>
-        <button>Send2</button>
-        <Tooltip
-          tooltipText="Tooltip"
-          tooltipSide="right"
-        />
+      <div className={css(styles.containerWrapper, styles.title, themeStyles.defaultFont)}>
+        <p>Click to activate all tooltip above</p>
+        <div className={css(styles.buttonWrapper)}>
+          <Tooltip tooltipText="Activate All Tooltip" tooltipSide="top">
+            <button type="button" className={css(styles.button)} onClick={() => setActive(!active)}>Activate</button>
+          </Tooltip>
+        </div>
       </div>
-    </div>
+      <div className={css(styles.containerWrapper, styles.title, themeStyles.defaultFont)}>
+        <p>Set tooltip for all checkboxes</p>
+        <Tooltip tooltipText="Max word length=20" tooltipSide="right">
+          <input type="text" name="tooltip" id="tooltip" onChange={tooltipTextOnChange} />
+        </Tooltip>
+      </div>
 
-    <div className={css(styles.container)}>
-      <div className={css(styles.buttonWrapper)}>
-        <a href="">Link</a>
-        <Tooltip
-          tooltipText="Tooltip"
-          tooltipSide="right"
-        />
-      </div>
-    </div>
 
-    <div className={css(styles.container)}>
-      <div className={css(styles.buttonWrapper)}>
-        <a href="">Link</a>
-        <Tooltip
-          tooltipText="Tooltip Tooltip Tooltip Tooltip Tooltip Tooltip"
-          tooltipSide="right"
-        />
-      </div>
     </div>
-  </>
-);
+  );
+};
 
 export default TooltipExample;
