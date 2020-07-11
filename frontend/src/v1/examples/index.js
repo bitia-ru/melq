c8295840a124a4e364710d1c163546ca80b86630
Sergey <vertigomaill@gmail.com>
2020-07-11 11:52:40 +0300

    MELQ-63: Add Card and CardPost component

--- frontend/src/v1/examples/index.js
+++ frontend/src/v1/examples/index.js
@@ -2,12 +2,14 @@ import ButtonExample from './ButtonExample';
 import SwitchExample from './SwitchExample';
 import TooltipExample from './TooltipExample';
 import CounterExample from './CounterExample';
+import PostCardExample from './PostCardExample';
 
 const examples = {
   button: ButtonExample,
   switch: SwitchExample,
   tooltip: TooltipExample,
   counter: CounterExample,
+  post_card: PostCardExample,
 };
 
 export default examples;
