29629ff5f6d6ee2b63cd31bd21dc3b4fdf73f1bf
Svetlana Batova <sbatova@bitia.ru>
2020-09-11 17:26:46 +0300

    MELQ-30: Add scripts

---
+++ prepare_remote_rep.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if [ -n "$REMOTE_REPO" ]
+then
+  mkdir -p storage
+  cd ../storage
+  git clone $REMOTE_REPO posts
+else
+  mkdir -p posts
+  cd posts
+  git init --bare
+  cd ..
+  mkdir -p storage
+  cd ./storage
+  git clone ../posts
+fi
+cd posts
+if [ -n "$REMOTE_NAME" ]
+then
+  git remote set-url $REMOTE_NAME ./posts
+else
+  git remote set-url origin ./posts
+fi
+
