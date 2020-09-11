aec14059f45881e163d114d3132782d21d486c80
Svetlana Batova <sbatova@bitia.ru>
2020-09-11 17:03:40 +0300

    MELQ-30: Refactor tags(backend)

--- backend/app/models/tag.rb
+++ backend/app/models/tag.rb
@@ -1,2 +1,30 @@
 class Tag < ApplicationRecord
+  after_create do
+    notify_about_changes_to_channel 'create'
+  end
+
+  after_update do
+    notify_about_changes_to_channel 'update'
+  end
+
+  after_destroy do
+    notify_about_changes_to_channel 'destroy'
+  end
+
+  def notify_about_changes_to_channel(action)
+    EntitiesChannel.broadcast_to(
+      'all',
+      body: {
+        tag: JSON.parse(
+          ApplicationController.render(
+            partial: 'api/v1/tags/tag',
+            locals: {
+              tag: self,
+              destroy: action == 'destroy'
+            }
+          )
+        )
+      }
+    )
+  end
 end
--- backend/app/views/api/v1/tags/_tag.json.jbuilder
+++ backend/app/views/api/v1/tags/_tag.json.jbuilder
@@ -1,1 +1,3 @@
 json.merge! tag.attributes
+
+json.merge!(_destroy: true) if defined?(destroy) && destroy
