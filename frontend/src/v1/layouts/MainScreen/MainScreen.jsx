e64f4af96ce6b8a24284f51eac71f8d0cd0e47ac
Svetlana Batova <sbatova@bitia.ru>
2020-11-12 16:11:28 +0300

    MELQ-129: Switch MainScreen to use new panels and headers layouts

--- frontend/src/v1/layouts/MainScreen/MainScreen.jsx
+++ frontend/src/v1/layouts/MainScreen/MainScreen.jsx
@@ -2,10 +2,36 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { css, StyleSheet } from '../../aphrodite';
 import withModals, { ModalContainerContext } from '../../modules/modalable';
+import { closeUserSession } from '@/v1/utils/auth';
+
 import LogInForm from '../../forms/LogInForm/LogInForm';
-import TextHeader from './TextHeader';
+import AdminPanel from './panels/AdminPanel';
+import UserPanel from './panels/UserPanel';
+import EditModeHeader from './headers/EditModeHeader';
+import DefaultHeader from './headers/DefaultHeader';
 
 import './scroll_workaround.css';
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexFlow: 'column',
+    height: '100vh',
+    overflowX: 'hidden',
+  },
+  scrollable: { overflowY: 'auto' },
+  unscrollable: { overflowY: 'hidden' },
+  wrapper: {
+    display: 'grid',
+    gridTemplateColumns: '28% 72%',
+  },
+  mainContent: {
+    display: 'grid',
+    gridTemplateRows: '101px auto',
+    marginLeft: 51,
+    marginRight: 104,
+  },
+});
 
 class MainScreen extends React.PureComponent {
   modals() {
@@ -17,6 +43,32 @@ class MainScreen extends React.PureComponent {
     };
   }
 
+  constructor(props) {
+    super(props);
+    this.state = { editMode: false };
+  }
+
+  renderPanel = () => {
+    const { editMode } = this.state;
+    if (this.props.user) {
+      return (
+        <AdminPanel
+          editMode={editMode}
+          switchEditMode={() => this.setState({ editMode: !editMode })}
+          logOut={closeUserSession}
+        />
+      );
+    }
+    return <UserPanel signIn={() => this.props.history.push('#signin')} />;
+  };
+
+  renderHeader = () => {
+    if (this.state.editMode) {
+      return <EditModeHeader addNewPost={() => this.props.history.push('/new')} />;
+    }
+    return <DefaultHeader />;
+  };
+
   render() {
     const { children, header } = this.props;
 
@@ -27,19 +79,19 @@ class MainScreen extends React.PureComponent {
             <div
               className={
                 css(
-                  style.container,
-                  isModalShown ? style.unscrollable : style.scrollable,
+                  styles.container,
+                  isModalShown ? styles.unscrollable : styles.scrollable,
                 )
               }
             >
-              <div style={{ flex: 1 }}>
-                {
-                  header && (
-                    typeof header === 'string' || typeof header === 'number'
-                      ? <TextHeader title={header} /> : header
-                  )
-                }
-                {children && children}
+              <div className={css(styles.wrapper)}>
+                { this.renderPanel() }
+                <div className={css(styles.mainContent)}>
+                  { this.renderHeader() }
+                  <div>
+                    {children && children}
+                  </div>
+                </div>
               </div>
             </div>
           )
@@ -48,20 +100,5 @@ class MainScreen extends React.PureComponent {
     );
   }
 }
-
-const style = StyleSheet.create({
-  container: {
-    display: 'flex',
-    flexFlow: 'column',
-    height: '100vh',
-    overflowX: 'hidden',
-  },
-  scrollable: {
-    overflowY: 'auto',
-  },
-  unscrollable: {
-    overflowY: 'hidden',
-  },
-});
 
 export default withRouter(withModals(MainScreen));
