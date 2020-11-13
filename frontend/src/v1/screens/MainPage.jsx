9ab0ef018fd161df7234e875b629f12ae1e8f967
Svetlana Batova <sbatova@bitia.ru>
2020-11-13 13:27:36 +0300

    MELQ-146: Switch MainPage to PostCards

--- frontend/src/v1/screens/MainPage.jsx
+++ frontend/src/v1/screens/MainPage.jsx
@@ -1,43 +1,109 @@
 import React from 'react';
-import { withRouter, Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import * as R from 'ramda';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import MainScreen from '../layouts/MainScreen/MainScreen';
+
+import { css, StyleSheet } from '../aphrodite';
+
 import { currentUser } from '@/v1/redux/user_session/utils';
 import { loadPosts } from '@/v1/redux/posts/actions';
 
+import MainScreen from '../layouts/MainScreen/MainScreen';
+import PostCard from '../components/PostCard/PostCard';
+import Button from '../components/Button/Button';
+
+const mapIndexed = R.addIndex(R.map);
+
+const btns = [
+  {
+    text: 'Опубликованные',
+    style: 'info',
+  },
+  { text: 'Черновики' },
+];
+
+const styles = StyleSheet.create({
+  cardsWrapper: {
+    marginTop: -2,
+    marginLeft: -32,
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  defaultHeader: { marginTop: 12 },
+  editModeHeader: { marginTop: 30 },
+  btnsContainer: { width: 289 },
+});
 
 class MainPage extends React.PureComponent {
   componentDidMount() {
     this.props.loadPosts();
   }
 
+  onCardClick = (post, e, target) => {
+    e.stopPropagation();
+    switch (target) {
+    case 'like':
+      console.log('Click on like counter');
+      break;
+    case 'comment':
+      console.log('Redirect to comment');
+      break;
+    case 'share':
+      console.log('Share post');
+      break;
+    default:
+      this.props.history.push(`/${post.slug}`);
+    }
+  };
+
   render() {
-    const { user, posts } = this.props;
+    const { user, posts, editMode } = this.props;
 
     return (
       <MainScreen header="" user={user}>
-        {
-          R.map(
-            post => (
-              <div key={post.slug}>
-                <h2>{post.title}</h2>
-                <div>{post.updated_at}</div>
-                <div>{`Tags: ${R.join(', ', R.map(tag => tag.text, post.tags))}`}</div>
-                <div>{`Likes: ${post.num_of_likes || 0}`}</div>
-                <div>{`Reposts: ${post.num_of_reposts || 0}`}</div>
-                <div>{`Views: ${post.num_of_views || 0}`}</div>
-                <div>
-                  <Link to={`/${post.slug}`}>Open</Link>
-                  |
-                  <Link to={`/${post.slug}/edit`}>Edit</Link>
-                </div>
+        <div className={css(styles.defaultHeader, editMode && styles.editModeHeader)}>
+          {
+            editMode && (
+              <div className={css(styles.btnsContainer)}>
+                <Button
+                  onClick={() => {}}
+                  size="big"
+                  tooltipText={R.map(btn => btn.tooltipText, btns)}
+                  tooltipSide={R.map(btn => btn.tooltipSide, btns)}
+                  disabled={R.map(btn => btn.disabled, btns)}
+                  btnStyle={R.map(btn => btn.style, btns)}
+                  isWaiting={R.map(btn => btn.isWaiting, btns)}
+                >
+                  {
+                    mapIndexed(
+                      (btn, index) => (
+                        <div key={index}>{btn.text}</div>
+                      ),
+                      btns,
+                    )
+                  }
+                </Button>
               </div>
-            ),
-            R.values(posts),
-          )
-        }
+            )
+          }
+        </div>
+        <div className={css(styles.cardsWrapper)}>
+          {
+            R.map(
+              post => (
+                <PostCard
+                  post={post}
+                  onClick={(e, target) => this.onCardClick(post, e, target)}
+                  disabledCounter={false}
+                  key={post.id}
+                />
+              ),
+              R.values(posts),
+            )
+          }
+        </div>
       </MainScreen>
     );
   }
@@ -46,11 +112,15 @@ class MainPage extends React.PureComponent {
 MainPage.propTypes = {
   user: PropTypes.object,
   posts: PropTypes.object,
+  editMode: PropTypes.bool,
+  history: PropTypes.object,
+  loadPosts: PropTypes.func,
 };
 
 const mapStateToProps = state => ({
   user: currentUser(state),
   posts: state.postsStoreV1.posts,
+  editMode: state.editMode,
 });
 
 const mapDispatchToProps = dispatch => ({ loadPosts: () => dispatch(loadPosts()) });
