20c607f74222af46e9b07114e48d896ad50fb875
Svetlana Batova <sbatova@bitia.ru>
2020-11-13 16:00:21 +0300

    MELQ-148: Exchange PostShow to fit design

--- frontend/src/v1/screens/PostShow.jsx
+++ frontend/src/v1/screens/PostShow.jsx
@@ -1,19 +1,102 @@
 import React from 'react';
-import { withRouter, Link } from 'react-router-dom';
+import dayjs from 'dayjs';
+import { withRouter } from 'react-router-dom';
 import * as R from 'ramda';
 import marked from 'marked';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import MainScreen from '../layouts/MainScreen/MainScreen';
-import { notReady, notExist } from '@/v1/utils';
 import { currentUser } from '@/v1/redux/user_session/utils';
-import { closeUserSession } from '@/v1/utils/auth';
 import { loadPost } from '@/v1/redux/posts/actions';
 import { loadComments } from '@/v1/redux/comments/actions';
-import Button from '@/v1/components/Button/Button';
 import prepareImageUrls from '@/v1/utils/prepareImageUrls';
 import PostComments from '../components/PostComments/PostComments';
+import Button from '../components/Button/Button';
+import LikeCounter from '../components/icon_counters/LikeCounter/LikeCounter';
+import CommentCounter from '../components/icon_counters/CommentCounter/CommentCounter';
+import Link from '../components/Link/Link';
+import TwoColumnsLayout from '../layouts/TwoColumnsLayout';
+import ShareCounter from '../components/icon_counters/ShareCounter/ShareCounter';
+import ViewCounter from '../components/icon_counters/ViewCounter/ViewCounter';
+import Item from '../components/Item/Item';
 
+import { StyleSheet, css } from '../aphrodite';
+
+import { bgColor, separatorColor, themeStyles, dateTopicCounterColor } from '../theme';
+
+const styles = StyleSheet.create({
+  container: { marginTop: 40 },
+  mainContentWrapper: { paddingBottom: 80 },
+  rightBlock: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  mainContent: {
+    backgroundColor: bgColor,
+    paddingLeft: 62,
+    paddingRight: 56,
+    paddingTop: 32,
+    paddingBottom: 32,
+    marginBottom: 32,
+  },
+  commentBlock: {
+    marginTop: 32,
+    backgroundColor: bgColor,
+    paddingLeft: 62,
+    paddingRight: 56,
+    paddingTop: 32,
+    paddingBottom: 32,
+  },
+  deleteBtnWrapper: {
+    textAlign: 'center',
+    marginTop: 24,
+    marginBottom: 24,
+  },
+  likeCounterContainer: {
+    width: 72,
+    paddingTop: 21,
+    paddingRight: 10,
+    paddingLeft: 17,
+    paddingBottom: 21,
+    border: `1px solid ${separatorColor}`,
+  },
+  commentCounterContainer: {
+    width: 72,
+    paddingTop: 21,
+    paddingRight: 10,
+    paddingLeft: 17,
+    paddingBottom: 19,
+    border: `1px solid ${separatorColor}`,
+    borderTopWidth: 0,
+  },
+  deleteBtnLinkText: {
+    marginLeft: 7,
+    marginTop: 1,
+  },
+  deleteBtnInnerContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  editBtnText: { marginLeft: 7 },
+  topInfoBlock: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  shareCounterWrapper: { marginLeft: 31 },
+  viewCounterWrapper: { marginLeft: 32 },
+  dateText: { color: dateTopicCounterColor },
+  postTitle: {
+    marginTop: 32,
+    marginBottom: 14,
+  },
+  topicBlock: {
+    borderTop: `1px solid ${separatorColor}`,
+    paddingTop: 32,
+    display: 'flex',
+  },
+  itemWrapper: { marginRight: 32 },
+});
 
 class PostShow extends React.PureComponent {
   constructor(props) {
@@ -41,44 +124,111 @@ class PostShow extends React.PureComponent {
   };
 
   render() {
-    const { user, history, posts } = this.props;
+    const { user, posts, history, editMode } = this.props;
 
     const post = posts[this.props.match.params.slug];
 
     return (
-      <MainScreen header="">
-        {
-          (!notReady(user) && notExist(user))
-            ? (
-              <Button onClick={() => history.push('#signin')}>Войти</Button>
-            )
-            : (
-              <Button onClick={closeUserSession}>Выйти</Button>
-            )
-        }
-        {
-          post && (
-            <div key={post.id}>
-              <h2>{post.title}</h2>
-              <div>{post.updated_at}</div>
-              <div
-                dangerouslySetInnerHTML={
-                  { __html: marked(this.prepareImageUrls(post.content || '')) }
+      <MainScreen header="" user={user}>
+        <div className={css(styles.container)}>
+          <TwoColumnsLayout>
+            <div className={css(styles.mainContentWrapper)}>
+              <div className={css(styles.mainContent)}>
+                {
+                  post && (
+                    <div key={post.id}>
+                      <div className={css(styles.topInfoBlock)}>
+                        <span className={css(themeStyles.defaultFont, styles.dateText)}>
+                          {dayjs(post.created_at).format('DD.MM.YYYY')}
+                        </span>
+                        <div className={css(styles.shareCounterWrapper)}>
+                          <ShareCounter value={post.num_of_reposts} onClick={() => {}} />
+                        </div>
+                        <div className={css(styles.viewCounterWrapper)}>
+                          <ViewCounter value={post.num_of_views} />
+                        </div>
+                      </div>
+                      <div className={css(themeStyles.largeHeaderFont, styles.postTitle)}>
+                        {post.title}
+                      </div>
+                      <div
+                        dangerouslySetInnerHTML={
+                          { __html: marked(this.prepareImageUrls(post.content || '')) }
+                        }
+                      />
+                    </div>
+                  )
                 }
-              />
-              <div>{`Tags: ${R.join(', ', R.map(tag => tag.text, post.tags))}`}</div>
-              <div>{`Likes: ${post.num_of_likes || 0}`}</div>
-              <div>{`Reposts: ${post.num_of_reposts || 0}`}</div>
-              <div>{`Views: ${post.num_of_views || 0}`}</div>
-              <PostComments />
-              <div>
-                <Link to="/">Back</Link>
-                |
-                <Link to={`/${post.slug}/edit`}>Edit</Link>
+                <div className={css(styles.topicBlock)}>
+                  <div className={css(styles.itemWrapper)}>
+                    <Item
+                      onTriggered={() => {}}
+                      size="small"
+                      iconSrc={require('../examples/images/demo_image.png')}
+                      text="Тема 1"
+                    />
+                  </div>
+                  <div className={css(styles.itemWrapper)}>
+                    <Item
+                      onTriggered={() => {}}
+                      size="small"
+                      iconSrc={require('../examples/images/demo_image.png')}
+                      text="Тема 2"
+                    />
+                  </div>
+                  <div className={css(styles.itemWrapper)}>
+                    <Item
+                      onTriggered={() => {}}
+                      size="small"
+                      iconSrc={require('../examples/images/demo_image.png')}
+                      text="Тема 3"
+                    />
+                  </div>
+                </div>
               </div>
+              <PostComments />
             </div>
-          )
-        }
+            <div className={css(styles.rightBlock)}>
+              {
+                editMode && (
+                  <div className={css(styles.btnContainer)}>
+                    <Button onClick={() => history.push(`/${post.slug}/edit`)}>
+                      <div>
+                        <svg width={16} height={16}>
+                          <use
+                            xlinkHref={`${require('../components/Button/images/edit.svg')}#edit`}
+                          />
+                        </svg>
+                        <span className={css(styles.editBtnText)}>Редактировать</span>
+                      </div>
+                    </Button>
+                    <div className={css(styles.deleteBtnWrapper)}>
+                      <Link onTriggered={() => {}}>
+                        <div className={css(styles.deleteBtnInnerContainer)}>
+                          <svg width={16} height={16}>
+                            <use xlinkHref={`${require('../components/Link/images/trash.svg')}#trash`} />
+                          </svg>
+                          <span className={css(styles.deleteBtnLinkText)}>Удалить</span>
+                        </div>
+                      </Link>
+                    </div>
+                  </div>
+                )
+              }
+              <div className={css(styles.likeCounterContainer)}>
+                <LikeCounter onClick={() => {}} value={post?.num_of_likes} />
+              </div>
+              <div className={css(styles.commentCounterContainer)}>
+                <CommentCounter onClick={() => {}} value={post?.num_of_comments} />
+              </div>
+              {
+                !editMode && (
+                  <div />
+                )
+              }
+            </div>
+          </TwoColumnsLayout>
+        </div>
       </MainScreen>
     );
   }
@@ -91,11 +241,13 @@ PostShow.propTypes = {
   match: PropTypes.object,
   history: PropTypes.object,
   loadComments: PropTypes.func,
+  editMode: PropTypes.bool,
 };
 
 const mapStateToProps = state => ({
   user: currentUser(state),
   posts: state.postsStoreV1.posts,
+  editMode: state.editMode,
 });
 
 const mapDispatchToProps = dispatch => ({
