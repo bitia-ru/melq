6df1da416ffee14233207afad4f2d3108dc8f95a
Svetlana Batova <sbatova@bitia.ru>
2020-11-20 12:34:52 +0300

    MELQ-154: Introduce ThemesIndex

--- frontend/src/v1/V1.jsx
+++ frontend/src/v1/V1.jsx
@@ -9,6 +9,7 @@ import PostEdit from './screens/PostEdit/PostEdit';
 import { currentUser as currentUserObtainer } from './redux/user_session/utils';
 import { default as MainExamplePage } from './examples/Main';
 import examples from '@/v1/examples';
+import ThemesIndex from './screens/ThemesIndex';
 
 const V1 = ({ currentUser }) => (
   <>
@@ -17,6 +18,7 @@ const V1 = ({ currentUser }) => (
         <Switch>
           <Route exact path="/" component={MainPage} />
           <Route exact path="/components" component={MainExamplePage} />
+          <Route exact path="/themes" component={ThemesIndex} />
           <Route exact path={['/new', '/:slug/edit']} component={PostEdit} />
           <Route exact path="/:slug" component={PostShow} />
           <Route
