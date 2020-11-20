108b319ee44f4cce304d0716b4393681bc802cf5
Svetlana Batova <sbatova@bitia.ru>
2020-11-20 12:34:52 +0300

    MELQ-154: Introduce ThemesIndex

--- frontend/src/v1/redux/tags/actions.js
+++ frontend/src/v1/redux/tags/actions.js
@@ -1,3 +1,4 @@
+import * as R from 'ramda';
 import Api from '../../utils/Api';
 import toastHttpError from '@/v1/utils/toastHttpError';
 import acts from './acts';
@@ -18,6 +19,56 @@ export const loadTags = () => ( // eslint-disable-line import/prefer-default-exp
         },
         failed(error) {
           dispatch({ type: acts.LOAD_TAGS_FAILED });
+
+          toastHttpError(error);
+        },
+      },
+    );
+  }
+);
+
+export const createTag = (attributes, afterSuccess, afterAll) => (
+  (dispatch) => {
+    dispatch({ type: acts.LOAD_TAGS_REQUEST });
+
+    Api.post(
+      '/v1/tags',
+      attributes,
+      {
+        dispatch,
+        method: 'post',
+        success() {
+          afterSuccess && afterSuccess();
+          afterAll && afterAll();
+        },
+        failed(error) {
+          dispatch({ type: acts.LOAD_TAGS_FAILED });
+          afterAll && afterAll();
+
+          toastHttpError(error);
+        },
+      },
+    );
+  }
+);
+
+export const removeTags = (ids, afterSuccess, afterAll) => (
+  (dispatch) => {
+    dispatch({ type: acts.LOAD_TAGS_REQUEST });
+
+    Api.post(
+      '/v1/tags',
+      { ids, tags: R.map(id => ({ id }), ids) },
+      {
+        dispatch,
+        method: 'delete',
+        success() {
+          afterSuccess && afterSuccess();
+          afterAll && afterAll();
+        },
+        failed(error) {
+          dispatch({ type: acts.LOAD_TAGS_FAILED });
+          afterAll && afterAll();
 
           toastHttpError(error);
         },
