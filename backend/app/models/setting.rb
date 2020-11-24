b8bb99da545e0c22751d5e70adb6de399248a46c
Svetlana Batova <sbatova@bitia.ru>
2020-11-24 14:27:27 +0300

    MELQ-165: Prepare backend for settings update

--- backend/app/models/setting.rb
+++ backend/app/models/setting.rb
@@ -1,6 +1,20 @@
 class Setting < ApplicationRecord
   has_one_attached :avatar
 
+  after_update do
+    EntitiesChannel.broadcast_to(
+      'all',
+      setting: JSON.parse(
+        ApplicationController.render(
+          partial: 'api/v1/settings/setting',
+          locals: {
+            setting: self
+          }
+        )
+      )
+    )
+  end
+
   def self.get
     find(1)
   end
