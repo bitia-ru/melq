8e2c747539c700f872ecfbd1e7cab2134820a374
Svetlana Batova <sbatova@bitia.ru>
2020-11-26 16:06:09 +0300

    MELQ-178: Add localForage for saving global state data

--- frontend/src/v1/store.js
+++ frontend/src/v1/store.js
@@ -1,10 +1,61 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import localForage from 'localforage';
 import rootReducer from './reducers';
 
-const store = createStore(
-  rootReducer,
-  applyMiddleware(thunk),
+import { INITIAL_VALUE as POSTS_DEFAULT_STORE_FORMAT } from '@/v1/redux/posts/reducer';
+
+export const saveState = (state) => {
+  try {
+    const data = {};
+    data.editMode = state.editMode;
+    data.selectedTagsIds = state.selectedTagsIds;
+    data.posts = state.postsStoreV1.posts;
+
+    localForage.setItem('reduxState', data);
+  } catch (err) {
+    console.log('Error saving redux state', err);
+  }
+};
+
+const getDataFromLocalForagePromise = () => (
+  new Promise((resolve) => {
+    localForage.getItem('reduxState', (err, data) => {
+      if (err) {
+        resolve(undefined);
+      } else {
+        const state = {};
+
+        try {
+          state.editMode = data.editMode;
+          state.selectedTagsIds = data.selectedTagsIds;
+          state.postsStoreV1 = POSTS_DEFAULT_STORE_FORMAT;
+          state.postsStoreV1.posts = data.posts;
+
+          resolve(state);
+        } catch (_err) {
+          resolve(undefined);
+        }
+      }
+    });
+  })
 );
 
-export default store;
+async function getDataFromLocalForage() {
+  const state = await getDataFromLocalForagePromise();
+  return state;
+}
+
+export const configureStoreAsync = () => (
+  new Promise((resolve) => {
+    try {
+      getDataFromLocalForage().then((state) => {
+        const store = createStore(rootReducer, state, applyMiddleware(thunk));
+        resolve(store);
+      });
+    } catch (error) {
+      const store = createStore(rootReducer, undefined, applyMiddleware(thunk));
+      resolve(store);
+    }
+  })
+);
